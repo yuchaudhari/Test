4852ba624d77e79f16be31191cdfeacade3ba941
Yuvaraj chaudhari <yuvi.chaudhari89@gmail.com>
2019-09-22 21:15:35 +0530

    new change

--- src/com/java8/ImplementationFunctional.java
+++ src/com/java8/ImplementationFunctional.java
@@ -5,7 +5,7 @@ public class ImplementationFunctional {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//u can not add parameter in that bracket
-		//it will give signature does not match with signature of run method
+		
 		Runnable run = () ->  
 			System.out.println("Thread");
 			System.out.println("Bye");
